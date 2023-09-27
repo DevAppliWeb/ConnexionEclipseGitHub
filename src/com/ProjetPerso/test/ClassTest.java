8602d1850f8cb1fb7f5b811d2938a00e9a45ceba
DevAppliWeb <dev.appli.web@gmail.com>
2023-09-27 12:33:19 +0200

    test commit

--- src/com/ProjetPerso/test/ClassTest.java
+++ src/com/ProjetPerso/test/ClassTest.java
@@ -3,7 +3,7 @@ package com.ProjetPerso.test;
 public class ClassTest {
 
 	public static void main(String[] args) {
-		System.out.println("test de connexion entre Eclipse et GitHub");
+		System.out.println("test de connexion entre Eclipse et GitHub");//test
 
 	}
 
