f81faf71e8d4b7786f390d34a6c26ea9b3973a04
DevAppliWeb <dev.appli.web@gmail.com>
2023-09-27 14:29:51 +0200

    test commit and push via token

--- src/com/ProjetPerso/test/ClassTest.java
+++ src/com/ProjetPerso/test/ClassTest.java
@@ -3,8 +3,8 @@ package com.ProjetPerso.test;
 public class ClassTest {
 
 	public static void main(String[] args) {
+		//test connexion
 		System.out.println("test de connexion entre Eclipse et GitHub");//test
-
 	}
 
 }
