80276faa6790cb4585d21b02248e44ccd1eb0915
DevAppliWeb <dev.appli.web@gmail.com>
2023-09-27 12:06:33 +0200

    Premier commit du projet Test. Affichage du message console. "test de connexion entre Eclipse et GitHub"

---
+++ src/com/ProjetPerso/test/ClassTest.java
@@ -0,0 +1,10 @@
+package com.ProjetPerso.test;
+
+public class ClassTest {
+
+	public static void main(String[] args) {
+		System.out.println("test de connexion entre Eclipse et GitHub");
+
+	}
+
+}
